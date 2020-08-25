6923820130505edb9479603890b47ce1d22c1493
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-08-25 15:13:35 +0530

    Adding: Printing stake = initial_stake

--- gambler.py
+++ gambler.py
@@ -24,9 +24,31 @@ class Gambler:
 
 
     def print_money(self, day, win_money, loss_money):
-        if day >=20:
+        if day > 20:
             print("Win Money on", day, "day is", win_money)
             print("Money Lost on", day, "day is", loss_money)
+            print()
 
 gambler = Gambler()
 initial_stake = gambler.start_gamble()
+stake = initial_stake
+days = 0
+win_money = 0
+loss_money = 0
+while days <= 29:
+    days += 1
+    while True:
+        bet = random.random()
+        if bet >= 0.5:
+            stake = gambler.win(stake)
+            win_money += 1
+        else:
+            stake = gambler.loose(stake)
+            loss_money += 1
+        if gambler.check_resign(initial_stake, stake):
+            break
+    gambler.print_money(days, win_money, loss_money)
+    win_money = 0
+    loss_money = 0
+#     print("Stake on", days," day", initial_stake)
+# print(days)
